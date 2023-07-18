b2f713464eaf794725b04830eca66529671814a3
TotallyNotAPumpkin <leopardcloudew@gmail.com>
2023-07-18 14:36:09 -0400

    created main

--- main.cpp
+++ main.cpp
@@ -1,12 +1,36 @@
 #include <iostream>
 
+int add(int x, int y)
+{
+  return x + y;
+}
+
+int subtract(int x, int y) {
+  return x - y;
+}
+
+int multiply(int x, int y) {
+  return x * y;
+}
+
+int divide(int x, int y) {
+  return x / y;
+}
+
+
 int main()
 {
-  std::cout << "Hello, world!" << std::endl;
+  std::cout << "Emily Wang 115" << std::endl;
 
-  int x = 5;
-  int y = 7;
-  std::cout << "The sum of " << x << " and " << y << " is " << x + y << std::endl;
-
+  int x = 8;
+  int y = 4;
+  int z = 3;
+  int w = 4;
+  int v = 1;
+  std::cout << "The sum of " << x << " and " << y << " is " << add(x, y) << std::endl;
+  std::cout << "The sum of " << x << ", " << y << ", and " << z << " is " << add(x, add(y, z)) << std::endl;
+  std::cout << "The product of " << x << ", " << y << ", " << w << ", and " << z << " is " << multiply(multiply(y, w), z) << std::endl;
+  std::cout << "The quotient of " << x << ", " << y << ", and " << v << " is " << divide(divide(x, y), v) << std::endl;
   return 0;
 }
+
